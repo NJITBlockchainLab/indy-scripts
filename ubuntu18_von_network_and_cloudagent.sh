08f14cffd73329c1b51e5ac6e2aa50501a9d1c61
nsgorlew <68826772+nsgorlew@users.noreply.github.com>
2022-02-10 16:00:26 -0500

    Update ubuntu18_von_network_and_cloudagent.sh

--- ubuntu18_von_network_and_cloudagent.sh
+++ ubuntu18_von_network_and_cloudagent.sh
@@ -22,7 +22,7 @@ sudo usermod -aG docker ${USER}
 newgrp docker
 docker run hello-world
 #install docker compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 #clone the von-network repo
