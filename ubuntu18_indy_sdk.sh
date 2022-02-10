d5a97deb796ecfa1d16a2da30d2846a79a5abb61
nsgorlew <68826772+nsgorlew@users.noreply.github.com>
2022-02-10 15:54:49 -0500

    Update ubuntu18_indy_sdk.sh

--- ubuntu18_indy_sdk.sh
+++ ubuntu18_indy_sdk.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
 #install rust
 curl https://sh.rustup.rs -sSf | sh
-
 #install nodejs package manager
 sudo apt install npm
-
 #install indy-sdk and its tools
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CE7709D068DB5E88
 sudo add-apt-repository "deb https://repo.sovrin.org/sdk/deb bionic stable"
@@ -14,4 +11,3 @@ sudo apt-get install -y libindy
 sudo apt-get install -y libnullpay
 sudo apt-get install -y libvcx
 sudo apt-get install -y indy-cli
-
